777706324dda8e65db6abca1ac87cbab7314cb80
Mahesh Babu Lakavathu <maheshlakavathu048@gmail.com>
2026-03-15 21:57:21 +0530

    [136] [Time Beats: 99.95%] [Memory Beats: 95.39%] - LeetPush

---
+++ Single-Number.java
@@ -0,0 +1,9 @@
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int xor=0;
+4        for(int i=0;i<nums.length;i++){
+5             xor=xor^nums[i];
+6        }
+7        return xor;
+8    }
+9}
