74884a398df04a2a3fa5f8aa06a140892c7f9036
Mahesh Babu Lakavathu <maheshlakavathu048@gmail.com>
2026-03-17 15:45:31 +0530

    [53] [Time Beats: 99.88%] [Memory Beats: 44.25%] - LeetPush

---
+++ Maximum-Subarray.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public int maxSubArray(int[] nums) {
+3        int sum=0;
+4        int ans=nums[0];
+5        for(int i=0;i<nums.length;i++){
+6            sum+=nums[i];
+7            ans=Math.max(ans,sum);
+8            if(sum<0){
+9                sum=0;
+10            }
+11        }
+12        return ans;
+13    }
+14}
