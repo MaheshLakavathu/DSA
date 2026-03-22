217a0446111d7783217db4704c0bd017e7f8c4a7
Mahesh Babu Lakavathu <maheshlakavathu048@gmail.com>
2026-03-22 14:14:47 +0530

    [Q1] [Time Beats: 84.55%] [Memory Beats: 99.75%] - LeetPush

---
+++ Q1.-Can-Make-Arithmetic-Progression-From-Sequence.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public boolean canMakeArithmeticProgression(int[] arr) {
+3        Arrays.sort(arr);
+4        int diff = arr[1] - arr[0];
+5        for(int i = 2; i < arr.length; i++){
+6            if(arr[i] - arr[i-1] != diff){
+7                return false;
+8            }
+9        }
+10        return true;
+11    }
+12}
